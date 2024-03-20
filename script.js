18ee44189fda26aa1a4a4e6bc8dcae2d4b0fb58e
Ashief <07ashief@gmail.com.com>
2024-03-20 15:51:57 +0530

    updated

--- script.js
+++ script.js
@@ -4,19 +4,29 @@ const count = document.querySelector(".count");
 const apply = document.querySelector(".inc");
 const reset = document.querySelector(".reset");
 
+updateButtons();
+function updateButtons() {
+  const countNum = parseInt(count.innerText);
+  minus.disabled = countNum <= 0;
+  plus.disabled = countNum >= 100;
+}
 
 minus.addEventListener("click", () => {
   let countNum = parseInt(count.innerText);
   let changeBy = parseInt(apply.value);
   count.innerText = countNum - changeBy;
+  updateButtons();
 });
+
 plus.addEventListener("click", () => {
   let countNum = parseInt(count.innerText);
   let changeBy = parseInt(apply.value);
   count.innerText = countNum + changeBy;
+  updateButtons();
 });
+
 reset.addEventListener("click", () => {
   count.innerText = 0;
   apply.value = 1;
+  updateButtons();
 });
-
